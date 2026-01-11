6d5520bb232a287525c040dd7c15b90e12423589
ArchanaBITS <archanasukaseelan93@gmail.com>
2026-01-11 23:50:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import joblib
+import os
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, matthews_corrcoef, classification_report
+from sklearn.preprocessing import StandardScaler
+
+st.set_page_config(page_title="Heart Disease Analysis")
+st.sidebar("Model Selection")
+model_options = ["logistic_reg", "decision_tree", "knn", "naive_bayes", "random_forest", "xgboost"]
+selected_model_name = st.sidebar.selectbox("Choose ML Model", model_options)
+
+Model_Dir = 'model'
 
