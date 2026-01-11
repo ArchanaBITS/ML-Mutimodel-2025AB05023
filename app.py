078c4e78142130856d39d28de42eaf7f6158176a
ArchanaBITS <archanasukaseelan93@gmail.com>
2026-01-12 00:31:32 +0530

    git commit -m"update changes"

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_sc
 from sklearn.preprocessing import StandardScaler
 
 st.set_page_config(page_title="Heart Disease Analysis")
-st.sidebar("Heart Disease Model Selection")
+st.sidebar.header("Heart Disease Model Selection")
 model_options = ["logistic_reg", "decision_tree", "knn", "naive_bayes", "random_forest", "xgboost"]
 selected_model_name = st.sidebar.selectbox("Choose ML Model", model_options)
 st.title("Heart Disease Model Analysis")
