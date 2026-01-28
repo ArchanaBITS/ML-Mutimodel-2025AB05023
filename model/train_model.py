21849dcc7cf243323b56005c2c7c40bf15d51986
ArchanaBITS <archanasukaseelan93@gmail.com>
2026-01-28 22:05:13 +0530

    Update train_model.py

--- model/train_model.py
+++ model/train_model.py
@@ -16,9 +16,13 @@ X = df.drop('target', axis=1)
 y = df['target']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-# Save them as CSVs
-train.to_csv('train.csv', index=False)
-test.to_csv('test.csv', index=False)
+# 3. Save CSVs (Fixed variable names)
+# We combine X and y so the CSV has the labels too
+train_df = pd.concat([X_train, y_train], axis=1)
+test_df = pd.concat([X_test, y_test], axis=1)
+
+train_df.to_csv('train.csv', index=False)
+test_df.to_csv('test.csv', index=False) # This saves in your root folder
 
 # Scaling
 scaler = StandardScaler()
